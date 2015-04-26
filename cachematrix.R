99f322c3995ac08270cef0e415f6d45d31ae39bb
RTX1138 <ac17n11235813@hotmail.co.uk>
2015-04-26 14:43:34 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This source constructs a matrix that supports caching of the inverse.
+## It does via the two methods below.
+## The first, makeCacheMatrix, takes a raw matrix and "wraps" it in a wrapper which defines get / set
+## methods both on the matrix itself and its inverse.
+## The getinv method is the key one used for caching.
+## The second method, cacheSolve is the one that manages the caching.
 
-## Write a short comment describing this function
+## Usage: 
+## [1] call makeCacheMatrix to wrap a raw matrix; the matrix can be unassigned, since it can be set via set()
+## [2] call cacheSolve to get the inverse, returning the cached value is already found
+
+## Assumptions:
+## in accordance with the assignment rubric, it's assumed all matrices will be numeric, square invertible.
+
+## -----------------------------------------------------------------------------------------------------------
+## wrap a raw matrix and furnish it with 4 methods - get/set on data/inverse
+## if no matrix provided, it creates a default empty matrix
+## note that inv is initially set to NULL to correctly initiate the caching logic in cacheSolve
+## similarly, when assigning a new matrix, the inv must be set to null to cacheSolve recognises it needs
+## to solve afresh
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -15,8 +31,10 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## -----------------------------------------------------------------------------------------------------------
+## pass a CacheMatrix to this method to get the inverse returned.
+## if the method has previously been called on the matrix, then the cached value will be returned
+## whenever a new inverse is calculated, the setinv method is called on the CacheMatrix to update the cache.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -30,3 +48,5 @@ cacheSolve <- function(x, ...) {
     x$setinv(inv)
     inv
 }
+
+## -----------------------------------------------------------------------------------------------------------
